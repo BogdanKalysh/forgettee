25ce34437166b2f6c6367f3087e3dbd97cf32e9f
bodyaka <bogdankalysh20@gmail.com>
2025-05-30 20:49:42 +0300

    added basic main activity layout and some resources.

--- app/src/main/java/com/bkalysh/forgettee/activities/MainActivity.kt
+++ app/src/main/java/com/bkalysh/forgettee/activities/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.bkalysh.forgettee
+package com.bkalysh.forgettee.activities
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.bkalysh.forgettee.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
